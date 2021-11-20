4ddc1976cd1dd673d377f64a5d8ae3df73bcb045
Rafi Uzzaman <rafiyasir9@gmail.com>
2021-11-21 02:39:17 +0530

    changed path for build

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/posts", require("./routes/posts"));
 //Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
